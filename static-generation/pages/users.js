520ec677d7593aa8ac95d54770d6a3f26f7e18c3
alsfkl222 <alskfl222@kakao.com>
2022-04-14 13:13:00 +0900

    practice static generation

---
+++ static-generation/pages/users.js
@@ -0,0 +1,28 @@
+import User from '../components/user'
+
+function UserList({ users }) {
+  return (
+    <>
+      <h1>List of USERS</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default UserList;
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
